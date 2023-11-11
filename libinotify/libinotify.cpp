a9ed7c554ebeac608020409904e54a3a3d521a0c
Kacper Paczos <kacper-paczos@linux.pl>
2023-11-11 21:17:24 +0100

    Added some functions, added logs

--- libinotify/libinotify.cpp
+++ libinotify/libinotify.cpp
@@ -1,9 +1,60 @@
 #include "libinotify.hpp"
 #include "spdlog/spdlog.h"
-#include <regex>
 
 namespace inotify
 {
+    // private
+    void Watcher::observeFiles()
+    {
+        for (const auto& file : watchList) {
+            int wd = inotify_add_watch(fd, file.first.c_str(), IN_ALL_EVENTS);
+            if (wd < 0) {
+                spdlog::error("Failed to add watch for file: {}", file.first);
+                continue;
+            }
+            if (verbose) { // Show information if verbose is true
+                spdlog::info("Watching file: {}", file.first);
+            }
+        }
+
+        if(observerThread.joinable()){
+            observerThread.join();
+        }
+        observerThread = std::jthread([this](){ this->observeFiles(); });
+    }
+    
+    // public
+    Watcher::Watcher()
+    {
+        fd = 0;
+        try {
+            #ifdef NDEBUG
+                spdlog::set_level(spdlog::level::info); // Set global log level to info for release version
+                spdlog::info("Log level set to info for release version");
+                verbose = false;
+            #else
+                spdlog::set_level(spdlog::level::debug); // Set global log level to debug for developer version
+                spdlog::info("Log level set to debug for developer version");
+                verbose = true;
+            #endif
+        } catch (const spdlog::spdlog_ex& ex) {
+            spdlog::warn("Failed to set log level: {}", ex.what());
+        }
+
+        fd = inotify_init();
+        if (fd < 0) {
+            spdlog::error("Failed to initialize inotify.");
+            throw std::runtime_error("Failed to initialize inotify.");
+        }
+
+    }
+
+    Watcher::~Watcher()
+    {
+        spdlog::warn("Obiekt został usunięty"); // Log warning that the object has been deleted
+        spdlog::shutdown(); // Stop logging
+    }
+
     void Watcher::excludeFile(const std::string& file)
     {
         // Check if the watcher is in recursive mode
@@ -13,10 +64,19 @@ namespace inotify
         }
 
         // Exclude the file from the watch list if it's already there
-        auto it = std::find(watchList.begin(), watchList.end(), file);
-        if (it != watchList.end()) {
-            watchList.erase(it);
+        auto it = watchList.begin();
+        while (it != watchList.end()) {
+            if (it->first == file) {
+                it = watchList.erase(it);
+            } else {
+                ++it;
+            }
         }
+
+        if(observerThread.joinable()){
+            observerThread.join();
+        }
+        observerThread = std::thread([this](){ this->observeFiles(); });
     }
 
     void Watcher::fromFile(const std::string& file)
@@ -25,19 +85,28 @@ namespace inotify
         std::string line;
         while (std::getline(infile, line)) {
             if (line[0] == '@') {
-                auto it = std::find(watchList.begin(), watchList.end(), line.substr(1));
+                auto it = watchList.find(line.substr(1));
                 if (it == watchList.end()) {
-                    watchList.push_back(line.substr(1));
-                    spdlog::info("Added to watchlist: {}", line.substr(1));
+                    watchList[line.substr(1)] = std::queue<std::string>();
+                    if (verbose) { // Show information if verbose is true
+                        spdlog::info("Added to watchlist: {}", line.substr(1));
+                    }
                 }
             } else if (line[0] == '-') {
-                auto it = std::find(watchList.begin(), watchList.end(), line.substr(1));
+                auto it = watchList.find(line.substr(1));
                 if (it != watchList.end()) {
                     watchList.erase(it);
-                    spdlog::info("Removed from watchlist: {}", line.substr(1));
+                    if (verbose) { // Show information if verbose is true
+                        spdlog::info("Removed from watchlist: {}", line.substr(1));
+                    }
                 }
             }
         }
+
+        if(observerThread.joinable()){
+            observerThread.join();
+        }
+        observerThread = std::thread([this](){ this->observeFiles(); });
     }
 
     void Watcher::zero()
@@ -50,13 +119,20 @@ namespace inotify
         // Implementation of not processing any events whose filename matches the specified POSIX extended regular expression, case sensitive
         std::regex pattern_regex(pattern);
         for (auto it = watchList.begin(); it != watchList.end(); ) {
-            if (std::regex_match(it->string(), pattern_regex)) {
-                spdlog::info("Removed from watchlist: {}", it->string());
+            if (std::regex_match(it->first, pattern_regex)) {
+                if (verbose) { // Show information if verbose is true
+                    spdlog::info("Removed from watchlist: {}", it->first);
+                }
                 it = watchList.erase(it);
             } else {
                 ++it;
             }
         }
+
+        if(observerThread.joinable()){
+            observerThread.join();
+        }
+        observerThread = std::thread([this](){ this->observeFiles(); });
     }
 
     void Watcher::excludei(const std::string& pattern)
@@ -64,39 +140,63 @@ namespace inotify
         // Implementation of not processing any events whose filename matches the specified POSIX extended regular expression, case insensitive
         std::regex pattern_regex(pattern, std::regex::icase);
         for (auto it = watchList.begin(); it != watchList.end(); ) {
-            if (std::regex_match(it->string(), pattern_regex)) {
-                spdlog::info("Removed from watchlist: {}", it->string());
+            if (std::regex_match(it->first, pattern_regex)) {
+                if (verbose) { // Show information if verbose is true
+                    spdlog::info("Removed from watchlist: {}", it->first);
+                }
                 it = watchList.erase(it);
             } else {
                 ++it;
             }
         }
+
+        if(observerThread.joinable()){
+            observerThread.join();
+        }
+        observerThread = std::thread([this](){ this->observeFiles(); });
     }
 
-    void Watcher::recursive()
+    void Watcher::recursive(const std::string& path)
     {
-        // Set the recursive mode flag
-        this->recursiveMode = true;
-
         // Implementation of watching all subdirectories of any directories passed as arguments
         // Using C++23 and generators
         auto generator = [] (const std::filesystem::path& p) -> std::generator<std::filesystem::path> {
             if (std::filesystem::is_directory(p)) {
+                // Set the recursive mode flag
+                this->recursiveMode = true;
                 for (auto& entry : std::filesystem::recursive_directory_iterator(p)) {
                     co_yield entry.path();
                 }
+            } else if (std::filesystem::is_regular_file(p)) {
+                spdlog::warn("The provided path is a file, not a directory: {}", p.string());
+                co_yield p;
             }
         };
 
         for (auto& path : generator(this->path)) {
-            watchList.push_back(path);
-            spdlog::info("Added to watchlist: {}", path.string());
+            watchList[path.string()] = std::queue<std::string>();
+            if (verbose) { // Show information if verbose is true
+                spdlog::info("Added to watchlist: {}", path.string());
+            }
         }
+
+        if(observerThread.joinable()){
+            observerThread.join();
+        }
+        observerThread = std::thread([this](){ this->observeFiles(); });
     }
 
     void Watcher::timeout(int seconds)
     {
         // Implementation of listening only for the specified amount of seconds
+        spdlog::info("Timer set for {} seconds", seconds);
+        std::this_thread::sleep_for(std::chrono::seconds(seconds));
+        spdlog::info("Timer of {} seconds has elapsed", seconds);
+        // Ensure the object is destroyed after the timer ends
+        if(observerThread.joinable()){
+            observerThread.join();
+        }
+        this->~Watcher();
     }
 
     void Watcher::event(const std::string& event)
@@ -113,5 +213,36 @@ namespace inotify
     {
         // Implementation of sorting output descending by event counts for the specified event
     }
+
+    bool Watcher::getVerbose() const
+    {
+        bool verboseValue = this->verbose;
+        spdlog::info("Verbose mode is currently set to: {}", verboseValue);
+        return verboseValue;
+    }
+
+    void Watcher::setVerbose(bool value)
+    {
+        if (!std::is_same<decltype(value), bool>::value) {
+            spdlog::error("Invalid argument. Expected a boolean value.");
+            throw std::invalid_argument("Invalid argument. Expected a boolean value.");
+        }
+        this->verbose = value;
+        getVerbose();
+        if (value) {
+            spdlog::set_level(spdlog::level::info); // Enable logging at info level
+        } else {
+            spdlog::set_level(spdlog::level::off); // Disable logging
+        }
+    }
+
+    std::map<std::string, std::queue<std::string>> Watcher::getCurrentEvents() const
+    {
+        std::map<std::string, std::queue<std::string>> currentEvents = this->fileEvents;
+        this->fileEvents.clear();
+        return currentEvents;
+    }
 }
+
+
 
--- libinotify/libinotify.hpp
+++ libinotify/libinotify.hpp
@@ -4,24 +4,37 @@
 #include <generator>
 #include <fstream>
 #include <iostream>
+#include <regex>
+#include <chrono>
+#include <thread>
 
 namespace inotify
 {
     class Watcher
     {
     private:
+        bool verbose; // Add verbose flag
         std::vector<std::filesystem::path> watchList;
-        recursiveMode=false;
+        std::map<std::filesystem::path, std::queue<std::string>> fileEvents; // Map where the first element is the path, and the second is a queue of events
+        std::thread observerThread;
+        bool recursiveMode=false;
+        void observeFiles();
+        int fd;
     public:
+        Watcher(); // Added constructor
+        ~Watcher(); // Added destructor
         void excludeFile(const std::string& file);
         void fromFile(const std::string& file);
         void zero();
         void exclude(const std::string& pattern);
         void excludei(const std::string& pattern);
-        void recursive();
+        void recursive(const std::string& path);
         void timeout(int seconds);
         void event(const std::string& event);
         void ascending(const std::string& event);
         void descending(const std::string& event);
+        bool setVerbose(bool value);
+        bool getVerbose() const;
+        std::vector<std::string> getCurrentEvents() const; // Function to return current file events
     };
 }
