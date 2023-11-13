243605674bc7cdf42bd3fadb7b0adcf9b744c811
Kacper Paczos <kacper-paczos@linux.pl>
2023-11-13 11:39:48 +0100

    Added example usage

--- example/main.cpp
+++ example/main.cpp
@@ -1,6 +1,41 @@
-#include <iostream>
+#include <libinotify/libinotify.hpp>
+#include <thread>
+#include <csignal>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+int main(int argc, char** argv)
+{
+  // Create a Watcher object
+  inotify::Watcher watcher;
+
+  // Set the directory to watch
+  std::filesystem::path path("/home/kacper/Dokumenty/obs");
+  watcher.recursive(path.string());
+
+  // Run this in a separate thread, the main thread closes when 'q' is pressed
+  std::atomic<bool> runWatcherThread(true);
+
+  std::thread watcherThread([&]() {
+    while (runWatcherThread) {
+      auto events = watcher.getCurrentEvents();
+      for (const auto& event : events) {
+        std::cout << "Event: " << event << std::endl;
+      }
+    }
+  });
+
+  // Handle 'q' input to terminate the program
+  char c;
+  while (std::cin >> c) {
+    if (c == 'q') {
+      std::cout << "\nProgram terminated by user\n";
+      runWatcherThread = false;
+      if (watcherThread.joinable()) {
+        watcherThread.join();
+      }
+      break;
+    }
+  }
+
+
+  return 0;
 }
